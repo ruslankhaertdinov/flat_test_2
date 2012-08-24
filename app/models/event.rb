cd293de27dfce236b1dcbc8c8404f2ea22bf854d
ruslan <ruslanimos@gmail.com>
2012-08-24 23:42:33 +0400

    some fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,9 +6,9 @@ class Event < ActiveRecord::Base
 
   validates_presence_of :user_id, :title, :date, message: 'поле является обязательным'
   validates_uniqueness_of :title, :scope => :user_id, message: 'дублирование события'
-  validates_length_of :title, in: 3..64, too_long: 'не более 64 символов', too_short: 'не менее 3 символов'
+  validates_length_of :title, :in => 3..64, too_long: 'не более 64 символов', too_short: 'не менее 3 символов'
   validates_inclusion_of :repeat, :in => 0..4, message: 'выберите значение из списка'
-  validates_format_of :date, :with => /\A\d{4}(\-\d{2}){2}\z/, :message => "в формате: '2012-12-31'"
+  validates_format_of :date, :with => /\A\d{4}(\-\d{2}){2}\z/, message: "в формате: '2012-12-31'"
 
   attr_accessible :title, :date, :repeat, :cal_day, :month, :weekday
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
+#coding=utf-8
 class User < ActiveRecord::Base
   has_many :events
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_length_of :name, :in => 3..64, :allow_blank => true, :too_long => 'не более 64 символов', :too_short => 'не менее 3 символов'
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
 end
