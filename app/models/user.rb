ace7ad216f232e0732a9f2c27fffaf3524bb2f5a
ruslan <ruslanimos@gmail.com>
2012-08-26 17:07:08 +0400

    user_test

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_length_of :name, in: 3..64, allow_blank: true, too_long: 'не более 64 символов', too_short: 'не менее 3 символов'
-  validates_length_of :email, maximum: 64, too_long: 'не более 64 символов'
+
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'validates_length_of :name, in: 3..64' do
+    user = User.new(email: 'valid@email.com', password: 'valid_password', password_confirmation: 'valid_password')
+    assert user.save
+
+    ['n' * 2, 'n' * 65].each do |n|
+      user.name = n
+      assert !user.save
+    end
+
+    ['n' * 3, 'n' *64].each do |n|
+      user.name = n
+      assert user.save
+    end
+  end
 end
